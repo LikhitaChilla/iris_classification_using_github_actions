22ca8ca3f1e4b060f548517d19b599df3728fa08
LikhitaChilla <chillalikhita1242@gmail.com>
2025-05-08 15:06:37 +0530

    i am sure it works now

--- src/train.py
+++ src/train.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
 iris=load_iris()
-x_train,y_train,x_test,y_test=train_test_split(iris.data,iris.target,test_size=0.3,random_state=42)
+x_train,x_test,y_train,y_test=train_test_split(iris.data,iris.target,test_size=0.3,random_state=42)
 
 
 mod=LogisticRegression()
